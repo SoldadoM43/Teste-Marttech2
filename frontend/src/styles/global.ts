c8661a625a67286bbf3f175af26e4a4fca3027b2
SoldadoM43 <d.novaes824@gmail.com>
2022-02-23 10:01:38 -0300

    global.js criado

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    heigth: 100%;
+`;
+
+export const Main = styled.div`
+    width: 300px;
+    heigth: 300px;
+    background-color: white;
+`;
