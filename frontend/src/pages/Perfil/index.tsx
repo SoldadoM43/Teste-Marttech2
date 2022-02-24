d82df440765b8ca572f53c8834d7f677937cb7b8
SoldadoM43 <d.novaes824@gmail.com>
2022-02-24 20:36:13 -0300

    Aba de Tarefas e Perfil Criadas

---
+++ frontend/src/pages/Perfil/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import {} from "./styles";
+
+import Menu from "../../components/Menu";
+
+export default function Perfil() {
+    return(
+        <>
+            <Menu />
+        </>
+    );
+}
---
+++ frontend/src/pages/Tarefas/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import {} from "./styles";
+
+import Menu from "../../components/Menu";
+
+export default function Tarefas() {
+    return(
+        <>
+            <Menu />
+        </>
+    );
+}
