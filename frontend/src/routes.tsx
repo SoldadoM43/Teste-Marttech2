6a01e83c08bc1eacad8d8a67b29f52cb33856aad
SoldadoM43 <d.novaes824@gmail.com>
2022-02-24 20:43:44 -0300

    Routes atualizado

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+import Perfil from "./pages/Perfil";
 import Home from "./pages/Home/index";
+import Tarefas from "./pages/Tarefas";
 import Login from "./pages/Telas/Login";
 import Senha from "./pages/Telas/Senha";
 import Cadastro from "./pages/Telas/Cadastro";
@@ -9,10 +11,12 @@ export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes >
-                <Route path="/home" element={<Home />} />
-                <Route path="/" element={<Login />} />
-                <Route path="/senha" element={<Senha />} />
-                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/" element={<Login />}/>
+                <Route path="/home" element={<Home />}/>
+                <Route path="/senha" element={<Senha />}/>
+                <Route path="/perfil" element={<Perfil />}/>
+                <Route path="/tarefas" element={<Tarefas />}/>
+                <Route path="/cadastro" element={<Cadastro />}/>
             </Routes>
         </BrowserRouter>
     );
