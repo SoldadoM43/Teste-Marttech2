e78d71fbaaf25d5d74ca5fd1f34663651a71a9a9
SoldadoM43 <d.novaes824@gmail.com>
2022-02-23 23:57:12 -0300

    Tela de Cadastro e Login Criada

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -1,4 +1,4 @@
-const app = require('../src/app');
+const app = require('../bin/src/app');
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,15 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Home from "./pages/Home/index";
+import Login from "./pages/Telas/Login";
+import Cadastro from "./pages/Telas/Cadastro";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes >
+                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
