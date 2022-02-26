616e89672b5e81675eab415a84eb5f412b8ef92d
SoldadoM43 <d.novaes824@gmail.com>
2022-02-26 19:24:42 -0300

    contextAPI criada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+
 import "./app.css";
+import { AuthContext } from "./providers/auth";
 import AppRoutes from './routes';
 
 function App() {
+    const {user} = React.useContext(AuthContext);
+    console.log({user});
     return (
         <AppRoutes />
     );
---
+++ frontend/src/providers/auth.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export const AuthContext = React.createContext({});
+
+export const AuthProvider = (props) => {
+    const [user, setUser] = useState ({
+        name: 'Diego',
+    })
+    return (
+        <AuthContext.Provider value={{user, setUser}}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => React.useContext(AuthContext);
