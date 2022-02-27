8c0830ff1ceeac28ea0348f3c8d9daf818785b75
SoldadoM43 <d.novaes824@gmail.com>
2022-02-26 21:53:25 -0300

    Cadastro criado

---
+++ frontend/src/pages/Telas/Cadastro/index.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { FaUserAlt, FaTasks } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+
+import "./styles.css";
+import { Container } from './styles';
+
+
+export default function Cadastro() {
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    return (
+        <>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Container>
+                <header>
+                    <h1 className="title">Cadastre-se</h1>
+                    <p className="subtitle">Conheça o TSS</p>
+                </header>
+                <FaTasks className="icon" />
+                <form
+                    onSubmit={event => {
+                        event.preventDefault();
+                        if (!!name) {
+                            setName('');
+                            setEmail('');
+                            setPassword('');
+                        }
+                        return;
+                    }}
+                ></form>
+                <div className="inputs">
+                    <table>
+                        <tr>
+                            <td><FaUserAlt /></td>
+                            <td>
+                                <input
+                                    type="text"
+                                    className="campo"
+                                    placeholder="Nome"
+                                    value={name}
+                                    onChange={event => setName(event.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><MdAlternateEmail /></td>
+                            <td>
+                                <input
+                                    type="email"
+                                    className="campo"
+                                    placeholder="E-mail"
+                                    value={email}
+                                    onChange={event => setEmail(event.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><RiLockPasswordFill /></td>
+                            <td>
+                                <input
+                                    type="password"
+                                    className="campo"
+                                    placeholder="Senha"
+                                    value={password}
+                                    onChange={event => setPassword(event.target.value)}
+                                />
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div className="botao">
+                    <Link to="/" className="link">
+                        <button type="submit" id="enter" className="btn">
+                            Cadastrar
+                        </button>
+                    </Link>
+                </div>
+            </Container>
+        </>
+    );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,18 +5,18 @@ import Home from "./pages/Home/index";
 // import Tarefas from "./pages/Tarefas";
 // import Login from "./pages/Telas/Login";
 // import Senha from "./pages/Telas/Senha";
-// import Cadastro from "./pages/Telas/Cadastro";
+import Cadastro from "./pages/Telas/Cadastro";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes >
                 {/* <Route path="/" element={<Login />}/> */}
-                <Route path="/home" element={<Home />}/>
+                <Route path="/" element={<Home />}/>
                 {/* <Route path="/senha" element={<Senha />}/> */}
                 <Route path="/perfil" element={<Perfil />}/>
                 {/* <Route path="/tarefas" element={<Tarefas/>}/> */}
-                {/* <Route path="/cadastro" element={<Cadastro />}/> */}
+                <Route path="/cadastro" element={<Cadastro />}/>
             </Routes>
         </BrowserRouter>
     );
